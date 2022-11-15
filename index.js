08556fb49a17af1c8b6d2c96d6be9b8722825c52
Steve Krenn <stevekrenn@gmail.com>
2022-11-14 21:40:22 -0600

    finished function for updating actor

--- index.js
+++ index.js
@@ -45,18 +45,60 @@ const addActor = async () => {
         }
     ])
 
-    try{
+    try {
         const [results] = await connection.promise().query(
             'INSERT INTO actor (first_name, last_name) VALUES (?, ?)',
             [answers.first_name, answers.last_name]
         )
         console.log(results)
-    } catch(error){
+    } catch (error) {
         throw new Error(error)
     }
-
-    
 }
+
+const updateActor = async () => {
+    //What actor do you want to update? ID
+    const answers = await inquirer.prompt([
+        {
+            type: 'number',
+            name: 'actor_id',
+            message: 'Enter ID of actor you want to update'
+        }, {
+            type: 'input',
+            name: 'first_name',
+            message: 'Update FIRST NAME:',
+            //Applies and enters default first name (ex: 'Josh' for 'Josh Mathers')
+            default: async (sessionAnswers) => {
+                const [results] = await connection.promise().query(
+                    'SELECT first_name FROM actor WHERE actor_id = ?',
+                    sessionAnswers.actor_id
+                )
+                return results[0].first_name
+            }
+        }, {
+            type: 'input',
+            name: 'last_name',
+            message: 'Update LAST NAME:',
+            //Applies and enters default last name (ex: 'Mathers' for 'Josh Mathers')
+            default: async (sessionAnswers) => {
+                const [results] = await connection.promise().query(
+                    'SELECT last_name FROM actor WHERE actor_id = ?',
+                    sessionAnswers.actor_id
+                )
+                return results[0].last_name
+            }
+        }
+    ])
+
+const [results] = await connection.promise().query(
+'UPDATE actor SET first_name = ?, last_name = ? WHERE actor_id = ?', 
+[answers.first_name, answers.last_name, answers.actor_id]
+)
+console.log('Actor updated!')
+menuPrompt()
+}
+
+
 
 const menuPrompt = async () => {
     const answers = await inquirer.prompt([
